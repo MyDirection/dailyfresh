263caa43d6f7fe61d2f045beb39f50bb59b15b2d
MyDirection <79077018+MyDirection@users.noreply.github.com>
2021-04-21 19:58:27 +0800

    Add files via upload

---
+++ apps/goods/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from goods.models import GoodsSKU, IndexGoodsBanner, GoodsType, IndexPromotionBanner
+
+admin.site.register(GoodsSKU)
+admin.site.register(IndexGoodsBanner)
+admin.site.register(GoodsType)
+admin.site.register(IndexPromotionBanner)
